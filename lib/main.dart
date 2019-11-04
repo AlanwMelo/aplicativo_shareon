a4459d26746bc81c4717a41039cc6b3af45ae82f
Alan Willian Melo <amelo967@gmail.com>
2019-11-04 17:21:47 -0300

    maps

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'models/usuario_model.dart';
+import 'package:google_map_location_picker/generated/i18n.dart' as location_picker;
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main (){
 
@@ -20,6 +22,16 @@ class _MyAppState extends State<MyApp> {
     return ScopedModel <UserModel>(
       model: UserModel(),
       child: MaterialApp(
+        localizationsDelegates: const [
+          location_picker.S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const <Locale>[
+          Locale('pt', ''),
+          Locale('en', ''),
+        ],
         title: "Share On",
         theme: ThemeData(
             primaryColor: Colors.indigo
