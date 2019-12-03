80c68bcff538e3c497adced1dfe5d6ce46c510b1
Alan Willian Melo <amelo967@gmail.com>
2019-12-03 15:15:16 -0300

    inserir e remover saldos (master)

---
+++ lib/layout_listas/lista_historico_meus_produtos.dart
@@ -0,0 +1,281 @@
+import 'package:aplicativo_shareon/telas/produto_selecionado.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class ListaHistoricoMeusProdutosBuilder extends StatefulWidget {
+  @override
+  _ListaHistoricoMeusProdutosBuilderState createState() => _ListaHistoricoMeusProdutosBuilderState();
+}
+
+class _ProductsHist {
+  String productID;
+  String name;
+  String media;
+  var preco;
+  String status;
+  Timestamp endDate;
+  String mainIMG;
+
+  _ProductsHist(this.productID, this.name, this.preco, this.media, this.status,
+      this.endDate, this.mainIMG);
+}
+
+class _ListaHistoricoMeusProdutosBuilderState extends State<ListaHistoricoMeusProdutosBuilder> {
+  SharedPreferencesController sharedPreferencesController =
+  new SharedPreferencesController();
+  final databaseReference = Firestore.instance;
+  String productID;
+  String status;
+  String userID = "";
+  int counter = 0;
+  List<_ProductsHist> _listaHistorico = [];
+  bool listIsEmpty = false;
+
+  @override
+  void initState() {
+    if (userID == "") {
+      sharedPreferencesController.getID().then(_setUserID);
+    }
+    super.initState();
+  }
+
+  getData() async {
+    await databaseReference
+        .collection("solicitations")
+        .where("ownerID", isEqualTo: userID)
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      if (snapshot.documents.length == 0) {
+        setState(() {
+          listIsEmpty = true;
+        });
+      }
+      snapshot.documents.forEach((f) {
+        Map productData = f.data;
+        if (productData["status"] == "concluido" ||
+            productData["status"] == "cancelada") {
+          listHelper(productData["productID"], productData["status"],
+              productData["finalEndDate"]);
+        }
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return _listaHistorico.length == 0 && listIsEmpty == false
+        ? Center(
+      child: CircularProgressIndicator(),
+    )
+        : listIsEmpty == true
+        ? Center(
+      child: Text(
+        "Você ainda não fez nenhuma transação",
+        style: TextStyle(
+          color: Colors.indigoAccent,
+          fontSize: 18,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    )
+        : listGen(_listaHistorico);
+  }
+
+  _onClick(BuildContext context, String idx) {
+    Navigator.push(context, MaterialPageRoute(builder: (BuildContext context) {
+      return ProdutoSelecionado(productID: idx);
+    }));
+  }
+
+//objetos
+
+  _img(String idx) {
+    return ClipRRect(
+      borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(16),
+          topRight: Radius.zero,
+          bottomRight: Radius.zero,
+          bottomLeft: Radius.circular(16)),
+      child: Container(
+        height: 150,
+        width: 150,
+        child: idx == null
+            ? Center(
+          child: CircularProgressIndicator(),
+        )
+            : Image.network(
+          idx,
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
+  }
+
+  _textNome(String idx, String status) {
+    Color color;
+    if (status == "concluido") {
+      color = Colors.indigoAccent;
+    } else {
+      color = Colors.redAccent;
+    }
+    return Expanded(
+      child: Text(
+        idx,
+        overflow: TextOverflow.ellipsis,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24,
+          color: color,
+        ),
+      ),
+    );
+  }
+
+  _textMedia(String idx) {
+    return Text(
+      idx,
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 16,
+        color: Colors.black54,
+      ),
+    );
+  }
+
+  _textData(Timestamp idx) {
+    int convertedDay = idx.toDate().day;
+    int convertedMonth = idx.toDate().month;
+    int convertedYear = idx.toDate().year;
+    String convertedTS =
+        "${convertedDay.toString().padLeft(2, "0")}/${convertedMonth.toString().padLeft(2, "0")}/$convertedYear";
+
+    return Text(
+      convertedTS,
+      style: TextStyle(
+          fontWeight: FontWeight.bold, fontSize: 16, color: Colors.black38),
+    );
+  }
+
+  _textPreco(var idx) {
+    return Text(
+      "R\$ $idx",
+      style: TextStyle(
+        fontWeight: FontWeight.bold,
+        fontSize: 14,
+      ),
+    );
+  }
+
+  _iconEstrela() {
+    return Icon(
+      Icons.star,
+      color: Colors.black54,
+      size: 20.0,
+    );
+  }
+
+  Widget listGen(List<_ProductsHist> _listaHist) {
+    return ListView.builder(
+      itemCount: _listaHist.length,
+      itemExtent: 150,
+      itemBuilder: (BuildContext context, int index) {
+        return GestureDetector(
+          onTap: () => _onClick(context, _listaHist[index].productID),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: BorderRadius.all(Radius.circular(16)),
+            ),
+            margin: EdgeInsets.all(6),
+            child: Row(
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                _img(_listaHist[index].mainIMG),
+                Expanded(
+                  child: Container(
+                    padding: EdgeInsets.all(12),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        _textNome(
+                            _listaHist[index].name, _listaHist[index].status),
+                        Container(
+                          margin: EdgeInsets.only(top: 8),
+                          child: Row(
+                            children: <Widget>[
+                              _textMedia(_listaHist[index].media),
+                              _iconEstrela(),
+                            ],
+                          ),
+                        ),
+                        Row(
+                          children: <Widget>[
+                            _textData(_listaHist[index].endDate),
+                            Expanded(
+                              child: Container(
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: <Widget>[
+                                    _textPreco(_listaHist[index].preco),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  _setUserID(String value) {
+    setState(() {
+      userID = value;
+      getData();
+    });
+  }
+
+  listHelper(String id, String status, Timestamp endDate) {
+    databaseReference
+        .collection("products")
+        .where("ID", isEqualTo: id)
+        .getDocuments()
+        .then((QuerySnapshot snapshot) {
+      snapshot.documents.forEach((f) async {
+        Map productData = f.data;
+
+        await databaseReference
+            .collection("productIMGs")
+            .where("productID", isEqualTo: productData["ID"])
+            .getDocuments()
+            .then((QuerySnapshot snapshot) {
+          snapshot.documents.forEach((f) {
+            Map productIMG = f.data;
+
+            _listaHistorico.add(new _ProductsHist(
+                id,
+                productData["name"],
+                productData["price"],
+                productData["media"],
+                status,
+                endDate,
+                productIMG["productMainIMG"]));
+            _listaHistorico.sort((b, a) => a.endDate.compareTo(b.endDate));
+            setState(() {});
+          });
+        });
+      });
+    });
+  }
+}
---
+++ lib/telas/funcoes_master.dart
@@ -0,0 +1,403 @@
+import 'package:aplicativo_shareon/utils/alter_debit.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_masked_text/flutter_masked_text.dart';
+import 'package:toast/toast.dart';
+import 'package:valida_cpf/valida_cpf.dart';
+
+class Master extends StatefulWidget {
+  @override
+  _MasterState createState() => _MasterState();
+}
+
+class _MasterState extends State<Master> {
+  final fieldsSaldo = GlobalKey<FormState>();
+  final databaseReference = Firestore.instance;
+  final alterSaldoCPF = new MaskedTextController(mask: '000.000.000-00');
+  final saldo = MoneyMaskedTextController();
+  String cpf;
+  double auxDouble;
+  bool loading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return loading == false
+        ? mater()
+        : Container(
+            color: Colors.white,
+            child: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+  }
+
+  Widget mater() {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Master'),
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Container(
+        child: Center(
+          child: Form(
+            key: fieldsSaldo,
+            child: SingleChildScrollView(
+              child: Container(
+                child: Column(
+                  children: <Widget>[
+                    Text(
+                      "Alterar saldo de créditos:",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.red,
+                        fontSize: 26,
+                        decoration: TextDecoration.none,
+                      ),
+                    ),
+                    Container(
+                      child: Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: Text(
+                              "Para alterar o saldo de créditos informe o valor e o CPF",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black87,
+                                fontSize: 16,
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(16),
+                      child: TextFormField(
+                        controller: alterSaldoCPF,
+                        decoration: InputDecoration(
+                          hintText: "CPF",
+                          alignLabelWithHint: true,
+                          labelStyle: TextStyle(color: Colors.black),
+                        ),
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        validator: (text) {
+                          if (text.length != 14) {
+                            return "O CPF deve conter 11 dígitos";
+                          }
+                          String aux = text.replaceAll(".", "");
+                          cpf = aux.replaceAll("-", "");
+
+                          if (validaCpf(cpf) == false) {
+                            return "CPF inválido";
+                          }
+                          if (text.isEmpty) {
+                            return "Campo CPF obrigatório";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(16),
+                      child: TextFormField(
+                        controller: saldo,
+                        keyboardType:
+                            TextInputType.numberWithOptions(decimal: true),
+                        validator: (text) {
+                          if (text.isEmpty || text == "0,00") {
+                            return "O valor não pode ser 0";
+                          } else {
+                            return null;
+                          }
+                        },
+                      ),
+                    ),
+                    Container(
+                      width: 400,
+                      margin: EdgeInsets.only(right: 16, left: 16),
+                      child: RaisedButton(
+                        color: Colors.indigoAccent,
+                        onPressed: () {
+                          if (fieldsSaldo.currentState.validate()) {
+                            String aux1 = saldo.text.replaceAll(".", "");
+                            String aux2 = aux1.replaceAll(",", ".");
+                            auxDouble = double.parse(aux2);
+                            alertExit(context, caller: 1);
+                          }
+                        },
+                        child: Text(
+                          "Inserir Saldo",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Container(
+                      width: 400,
+                      margin: EdgeInsets.only(right: 16, left: 16),
+                      child: RaisedButton(
+                        color: Colors.red,
+                        onPressed: () {
+                          if (fieldsSaldo.currentState.validate()) {
+                            alertExit(context, caller: 2);
+                            print(cpf);
+                            print(saldo.text);
+                          }
+
+                          print(saldo.text);
+                          String aux1 = saldo.text.replaceAll(".", "");
+                          String aux2 = aux1.replaceAll(",", ".");
+                          auxDouble = double.parse(aux2);
+                          print(auxDouble * -1);
+                        },
+                        child: Text(
+                          "Remover Saldo",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      thickness: 3,
+                    ),
+                    Container(
+                      width: 400,
+                      margin: EdgeInsets.only(right: 16, left: 16),
+                      child: RaisedButton(
+                        color: Colors.indigoAccent,
+                        onPressed: () {
+                          alertExit(context, caller: 3);
+                        },
+                        child: Text(
+                          "Inserir 10 avaliações",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      thickness: 3,
+                    ),
+                    Container(
+                      width: 400,
+                      margin: EdgeInsets.only(right: 16, left: 16),
+                      child: RaisedButton(
+                        color: Colors.indigoAccent,
+                        onPressed: () {
+                          alertExit(context, caller: 4);
+                        },
+                        child: Text(
+                          "Atualizar status das reservas",
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Divider(
+                      thickness: 3,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  alertExit(BuildContext context, {int caller}) {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return GestureDetector(
+          onTap: () => Navigator.pop(context),
+          child: Container(
+            color: Colors.white.withOpacity(0.1),
+            child: Center(
+              child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(16)),
+                child: GestureDetector(
+                  onTap: () => null,
+                  child: Container(
+                    color: Colors.white,
+                    height: 120,
+                    width: 300,
+                    child: Container(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(bottom: 8, top: 8),
+                            child: Text(
+                              "Confirmação",
+                              style: TextStyle(
+                                decoration: TextDecoration.none,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.redAccent,
+                                fontSize: 24,
+                              ),
+                            ),
+                          ),
+                          Text(
+                            "Deseja mesmo confirmar esta ação?",
+                            style: TextStyle(
+                              fontFamily: 'RobotoMono',
+                              color: Colors.black,
+                              fontSize: 16,
+                              fontWeight: FontWeight.bold,
+                              decoration: TextDecoration.none,
+                            ),
+                          ),
+                          Expanded(
+                            child: Container(
+                              color: Colors.indigoAccent,
+                              margin: EdgeInsets.only(
+                                top: 8,
+                              ),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Container(
+                                      height: 100,
+                                      child: RaisedButton(
+                                        color: Colors.indigoAccent,
+                                        onPressed: () {
+                                          confirmar(caller);
+                                          setState(() {
+                                            Navigator.pop(context);
+                                          });
+                                        },
+                                        child: Text(
+                                          "Confirmar",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Container(
+                                      height: 100,
+                                      child: RaisedButton(
+                                        color: Colors.indigoAccent,
+                                        onPressed: () {
+                                          Navigator.pop(context);
+                                        },
+                                        child: Text(
+                                          "Cancelar",
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                          ),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  confirmar(int caller) async {
+    if (caller == 1) {
+      setState(() {
+        loading = true;
+      });
+      await databaseReference
+          .collection("users")
+          .where("cpf", isEqualTo: cpf)
+          .getDocuments()
+          .then((QuerySnapshot snapshot) {
+        snapshot.documents.forEach((f) async {
+          Map usarData = f.data;
+
+          String aux = usarData["userID"];
+
+          Map<String, dynamic> debitADD = {
+            "userID": aux,
+            "reason": "altered by master",
+            "debit": auxDouble,
+            "statusTS": Timestamp.fromDate(DateTime.now()),
+          };
+
+          await databaseReference.collection("debitHist").add(debitADD);
+
+          var alter = AlterDebit(userID: aux);
+          alter.alterdebit();
+
+          alterSaldoCPF.text = "";
+
+          _toast("Créditos removidos com sucesso", context);
+        });
+      });
+      setState(() {
+        loading = false;
+      });
+    }
+    else if (caller == 2) {
+      setState(() {
+        loading = true;
+      });
+      await databaseReference
+          .collection("users")
+          .where("cpf", isEqualTo: cpf)
+          .getDocuments()
+          .then((QuerySnapshot snapshot) {
+        snapshot.documents.forEach((f) async {
+          Map usarData = f.data;
+
+          String aux = usarData["userID"];
+
+          Map<String, dynamic> debitADD = {
+            "userID": aux,
+            "reason": "altered by master",
+            "debit": auxDouble*-1,
+            "statusTS": Timestamp.fromDate(DateTime.now()),
+          };
+
+          await databaseReference.collection("debitHist").add(debitADD);
+
+          var alter = AlterDebit(userID: aux);
+          alter.alterdebit();
+
+          saldo.text = "";
+
+          _toast("Créditos inseridos com sucesso", context);
+        });
+      });
+      setState(() {
+        loading = false;
+      });
+    }
+    else if (caller == 3) {}
+    else if (caller == 4) {}
+  }
+
+  _toast(String texto, BuildContext context) {
+    Toast.show("$texto", context,
+        duration: 3,
+        gravity: Toast.BOTTOM,
+        backgroundColor: Colors.black.withOpacity(0.8));
+  }
+}
